e36a7ea2d33e7e60e6014a929bba896f96f0dacf
Tim Ewing <tiew0674@colorado.edu>
2018-12-12 19:21:25 +0000

    Working motor driver

--- src/csci3302_final/scripts/motor_driver.py
+++ src/csci3302_final/scripts/motor_driver.py
@@ -3,60 +3,67 @@
 import rospy
 import json
 import RPi.GPIO as GPIO
+from std_msgs.msg import String
 
 # GPIO pin stuff
 _PINS = {
-    'A_PWM' : 18,
-    'A_IN2' : 19,
-    'A_IN1' : 20,
-    'STDBY' : 21,
-    'B_IN1' : 22,
-    'B_IN2' : 23,
-    'B_PWM' : 24
+    'A_PWM' : 13,
+    'A_IN2' : 6,
+    'A_IN1' : 5,
+    'STDBY' : 22,
+    'B_IN1' : 27,
+    'B_IN2' : 17,
+    'B_PWM' : 4 
 }
+_BOT_ID = 1
 
 class Bot:
     def __init__(self):
-        self.motor_A_PWM = GPIO.PWM(_PINS['A_PWM'], 50)
-        self.motor_B_PWM = GPIO.PWM(_PINS['B_PWM'], 50)
-        self.motor_A_PWM.start(0)
-        self.motor_B_PWM.start(0)
-
         GPIO.setup(_PINS['A_IN2'], GPIO.OUT)
         GPIO.setup(_PINS['A_IN1'], GPIO.OUT)
         GPIO.setup(_PINS['STDBY'], GPIO.OUT)
         GPIO.setup(_PINS['B_IN1'], GPIO.OUT)
         GPIO.setup(_PINS['B_IN2'], GPIO.OUT)
 
+        GPIO.setup(_PINS['A_PWM'], GPIO.OUT)
+        GPIO.setup(_PINS['B_PWM'], GPIO.OUT)
+        self.motor_A_PWM = GPIO.PWM(_PINS['A_PWM'], 50)
+        self.motor_B_PWM = GPIO.PWM(_PINS['B_PWM'], 50)
+        self.motor_A_PWM.start(0)
+        self.motor_B_PWM.start(0)
+
         GPIO.output(_PINS['STDBY'], GPIO.HIGH)
 
     def parse_msg(self, msg):
-        msg = json.loads(msg)
-        self.set_A(msg['left'])
-        self.set_B(msg['right'])
+        msg = msg.data.split()
+        left, right = msg
+        self.set_A(int(left))
+        self.set_B(int(right))
 
     def set_A(self, speed=0):
+        print("Setting A to {}".format(speed))
         if speed > 0:
             GPIO.output(_PINS['A_IN1'], GPIO.HIGH)
             GPIO.output(_PINS['A_IN2'], GPIO.LOW)
-            self.motor_A.ChangeDutyCycle(left)
+            self.motor_A_PWM.ChangeDutyCycle(speed)
         elif speed < 0:
             GPIO.output(_PINS['A_IN1'], GPIO.LOW)
             GPIO.output(_PINS['A_IN2'], GPIO.HIGH)
-            self.motor_A.ChangeDutyCycle(-left)
+            self.motor_A_PWM.ChangeDutyCycle(-speed)
         else:
             GPIO.output(_PINS['A_IN1'], GPIO.LOW)
             GPIO.output(_PINS['A_IN2'], GPIO.LOW)
 
     def set_B(self, speed=0):
+        print("Setting B to {}".format(speed))
         if speed > 0:
             GPIO.output(_PINS['B_IN1'], GPIO.HIGH)
             GPIO.output(_PINS['B_IN2'], GPIO.LOW)
-            self.motor_B.ChangeDutyCycle(left)
+            self.motor_B_PWM.ChangeDutyCycle(speed)
         elif speed < 0:
             GPIO.output(_PINS['B_IN1'], GPIO.LOW)
             GPIO.output(_PINS['B_IN2'], GPIO.HIGH)
-            self.motor_B.ChangeDutyCycle(-left)
+            self.motor_B_PWM.ChangeDutyCycle(-speed)
         else:
             GPIO.output(_PINS['B_IN1'], GPIO.LOW)
             GPIO.output(_PINS['B_IN2'], GPIO.LOW)
